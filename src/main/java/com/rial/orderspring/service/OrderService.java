7899181bc0504b7922552ba29fc013de084af21e
sergioricart <sergiricartt@gmail.com>
2025-11-15 17:34:48 +0100

    Service, Respoitory

--- src/main/java/com/rial/orderspring/service/OrderService.java
+++ src/main/java/com/rial/orderspring/service/OrderService.java
@@ -1,4 +1,30 @@
 package com.rial.orderspring.service;
 
+import com.rial.orderspring.enums.OrderState;
+import com.rial.orderspring.model.Order;
+
+import java.time.LocalDateTime;
+import java.util.List;
+import java.util.Optional;
+
 public interface OrderService {
+
+
+    Order create(Order order);
+
+    List<Order> findAll();
+
+    Order findById(String id);
+
+    List<Order> findByOrderDateTime(LocalDateTime orderDateTime);
+
+    List<Order> findByDeliveryDateTime(LocalDateTime deliveryDateTime);
+
+    List<Order> findByOrderState(OrderState orderState);
+
+    List<Order> findByClientId(String clientId);
+    
+    Order update(String id, Order updatedOrder);
+
+    void deleteById(String id);
 }
--- src/main/java/com/rial/orderspring/service/impl/OrderServiceImpl.java
+++ src/main/java/com/rial/orderspring/service/impl/OrderServiceImpl.java
@@ -1,8 +1,66 @@
 package com.rial.orderspring.service.impl;
 
+import com.rial.orderspring.enums.OrderState;
+import com.rial.orderspring.exception.OrderNotFoundException;
+import com.rial.orderspring.model.Order;
+import com.rial.orderspring.repository.OrderRepository;
 import com.rial.orderspring.service.OrderService;
 import org.springframework.stereotype.Service;
 
+import java.time.LocalDateTime;
+import java.util.List;
+
 @Service
 public class OrderServiceImpl implements OrderService {
+
+    private final OrderRepository orderRepository;
+
+    public OrderServiceImpl(OrderRepository orderRepository) {
+        this.orderRepository = orderRepository;
+    }
+
+    @Override
+    public Order create(Order order) {
+        return orderRepository.save(order);
+    }
+
+    @Override
+    public List<Order> findAll() {
+        return orderRepository.findAll();
+    }
+
+    @Override
+    public Order findById(String id) {
+        return orderRepository.findById(id).orElseThrow(() -> new OrderNotFoundException(id));
+    }
+
+    @Override
+    public List<Order> findByOrderDateTime(LocalDateTime orderDateTime) {
+        return orderRepository.findByOrderDateTime(orderDateTime).orElseThrow(() -> new OrderNotFoundException(orderDateTime));
+    }
+
+    @Override
+    public List<Order> findByDeliveryDateTime(LocalDateTime deliveryDateTime) {
+        return orderRepository.findByDeliveryDateTime(deliveryDateTime).orElseThrow(() -> new OrderNotFoundException(deliveryDateTime));
+    }
+
+    @Override
+    public List<Order> findByOrderState(OrderState orderState) {
+        return orderRepository.findByOrderState(orderState).orElseThrow(() -> new OrderNotFoundException(orderState.name()));
+    }
+
+    @Override
+    public List<Order> findByClientId(String clientId) {
+        return null;
+    }
+
+    @Override
+    public Order update(String id, Order updatedOrder) {
+        return null;
+    }
+
+    @Override
+    public void deleteById(String id) {
+
+    }
 }
